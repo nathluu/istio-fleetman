7c0838c7c6e08acf40529db5921ace1157dac0cd
nathluu <luuhoangtanbis@gmail.com>
2021-03-15 09:21:02 +0700

    Create setupjumpvm.sh

---
+++ deployment/azure/multicluster/pluginCA/setupjumpvm.sh
@@ -0,0 +1,4 @@
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+curl -sL https://istio.io/downloadIstioctl | sh -
+curl -LO https://dl.k8s.io/release/v1.19.7/bin/linux/amd64/kubectl
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
