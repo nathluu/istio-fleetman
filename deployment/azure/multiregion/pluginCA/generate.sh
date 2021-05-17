c1fe034763f718368213f97256a9dab87e1372bf
Tan Luu <luuhoangtanbis@gmail.com>
2021-05-17 18:53:12 +0700

    Fix gen certificate scripts

--- deployment/azure/multiregion/pluginCA/generate.sh
+++ deployment/azure/multiregion/pluginCA/generate.sh
@@ -10,7 +10,7 @@ if [[ ! -f root-cert.pem ]];then
   make -f ../tools/certs/Makefile.selfsigned.mk root-ca
 fi
 
-ctxs=$(kubectl config view -o jsonpath='{.contexts[*].name}' | grep -v "docker-desktop" | sed 's/ /\n/g')
+ctxs=$(kubectl config view -o jsonpath='{.contexts[*].name}' | sed 's/ /\n/g' | grep -v "docker-desktop")
 for ctx in $ctxs
 do
   make -f ../tools/certs/Makefile.selfsigned.mk "$ctx-cacerts"
