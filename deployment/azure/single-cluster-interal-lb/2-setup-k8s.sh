59ba4d07b50795ef61d2726d7add7ea6e228a28a
nathluu <luuhoangtanbis@gmail.com>
2021-08-01 10:37:27 +0700

    Single cluster with internal LB

---
+++ deployment/azure/single-cluster-interal-lb/2-setup-k8s.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+CLUSTER_NETWORK=""
+CLUSTER="Kubernetes" #This is a fixed value
+
+# istioctl operator init
+
+cat <<EOF > vm-cluster.yaml
+apiVersion: install.istio.io/v1alpha1
+kind: IstioOperator
+metadata:
+  name: istio
+spec:
+  values:
+    global:
+      meshID: mesh1
+      multiCluster:
+        clusterName: "${CLUSTER}"
+      network: "${CLUSTER_NETWORK}"
+  meshConfig:
+    accessLogFile: /dev/stdout
+    enableTracing: true
+  components:
+    ingressGateways:
+    - name: istio-ingressgateway
+      enabled: true
+      k8s:
+        serviceAnnotations:
+          service.beta.kubernetes.io/azure-load-balancer-internal: "true"
+EOF
+
+istioctl install -f vm-cluster.yaml -y
+
+if ! kubectl apply -f addons/; then
+  sleep 5 && kubectl apply -f addons/
+fi
+
+# kubectl label namespace default istio-injection=enabled
